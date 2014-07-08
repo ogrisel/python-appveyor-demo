c63a6fd6fd49fd2c6dce8d1175586d2a3419d963
Olivier Grisel <olivier.grisel@ensta.org>
2014-07-08 18:02:09 +0200

    Support for SDK v7.0 SetEnv.cmd script

--- appveyor/run_with_env.cmd
+++ appveyor/run_with_env.cmd
@@ -5,12 +5,20 @@
 :: More details at:
 :: https://github.com/cython/cython/wiki/64BitCythonExtensionsOnWindows
 
+SET COMMAND_TO_RUN=%*
+
 IF "%PYTHON_ARCH%"=="64" (
     ECHO Configuring environment to build with MSVC on a 64bit architecture
-    ECHO Using Windows SDK %WINDOWS_SDK_VERSION%
-    "C:\Program Files\Microsoft SDKs\Windows\%WINDOWS_SDK_VERSION%\Bin\SetEnv.cmd" /x64 /release
     SET DISTUTILS_USE_SDK=1
     SET MSSdk=1
+    ECHO Using Windows SDK %WINDOWS_SDK_VERSION%
+    "C:\Program Files\Microsoft SDKs\Windows\%WINDOWS_SDK_VERSION%\Bin\SetEnv.cmd" /x64 /release
+    ECHO Executing: %COMMAND_TO_RUN%
+    call %COMMAND_TO_RUN% || EXIT 1
+    ECHO OK
 ) ELSE (
     ECHO Using default MSVC build environment for 32bit architecture
+    ECHO Executing: %COMMAND_TO_RUN%
+    call %COMMAND_TO_RUN% || EXIT 1
+    ECHO OK
 )
